1e1a49d7a07abe81b910171b333039412c4fa00b
Scott Seufert <scott.seufert@gmail.com>
2026-09-26 14:50:02 -0500

    Update macOS bundle configuration for Apple Silicon

--- osx/makebundle.sh
+++ osx/makebundle.sh
@@ -3,7 +3,7 @@
 rm -rf HexChat.app
 rm -f *.app.zip
 
-python $HOME/.local/bin/gtk-mac-bundler hexchat.bundle
+python3 /opt/homebrew/bin/gtk-mac-bundler hexchat.bundle
 
 echo "Compressing bundle"
 #hdiutil create -format UDBZ -srcdir HexChat.app -quiet HexChat-2.9.6.1-$(git rev-parse --short master).dmg
